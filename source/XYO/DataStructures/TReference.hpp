7493a6b4ca74a39a4d58b54ac348033644dd4dcc
Grigore Stefan <g_stefan@yahoo.com>
2023-01-10 12:14:11 +0200

    Update

--- source/XYO/DataStructures/TReference.hpp
+++ source/XYO/DataStructures/TReference.hpp
@@ -1,11 +1,8 @@
-//
 // Data Structures
-//
-// Copyright (c) Grigore Stefan <g_stefan@yahoo.com>
-// Created by Grigore Stefan <g_stefan@yahoo.com>
-//
+// Copyright (c) 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
 // MIT License (MIT) <http://opensource.org/licenses/MIT>
-//
+// SPDX-FileCopyrightText: 2016-2023 Grigore Stefan <g_stefan@yahoo.com>
+// SPDX-License-Identifier: MIT
 
 #ifndef XYO_DATASTRUCTURES_TREFERENCE_HPP
 #define XYO_DATASTRUCTURES_TREFERENCE_HPP
